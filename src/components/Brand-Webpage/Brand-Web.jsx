17ad2181055fe571ced022dad5d7b23e9418e7c1
Rajesh <rajeshbarik2006raja@gmail.com>
2025-04-29 14:10:07 +0530

    added brand web

---
+++ src/components/Brand-Webpage/Brand-Web.jsx
@@ -0,0 +1,30 @@
+const BrandWebpage = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img
+            src="/images/download.jpeg"
+            width={"40px"}
+            height={"30px"}
+            alt="logo"
+          />
+          <img
+            src="/images/shoes.jpg"
+            width={"300px"}
+            height={"200px"}
+            alt="Shoes"
+          />
+        </div>
+        <ul>
+          <li herf="#">Menu</li>
+          <li herf="#">Location</li>
+          <li herf="#">Contact</li>
+          <li herf="#">About</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+export default BrandWebpage;
